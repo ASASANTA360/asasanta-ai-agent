bc8d0b5e697f1acf9e44873c40d131b97e6ad4c1
SALISU ISHAQ <138232516+ASASANTA360@users.noreply.github.com>
2026-05-14 04:15:01 -0700

    Fix API key in sendMessage function

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ async function sendMessage() {
   try {
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyB9Qqha07-WnA9WrFPzSefFcL6SIT7CYUA",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBtcnpycwDmRkRxP2Jqjsm5av7QRECIB_c",
       {
         method: "POST",
 
